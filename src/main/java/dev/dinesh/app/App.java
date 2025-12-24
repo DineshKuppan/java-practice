1bf78ed8ef2c68740c255cf5648bf1cc536758ce
Dineshkumar, Kuppan <email2dineshkuppan@gmail.com>
2025-12-25 00:45:49 +0530

    initial codebase to create first java project (from scratch)

---
+++ src/main/java/dev/dinesh/app/App.java
@@ -0,0 +1,13 @@
+package dev.dinesh.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, Java! Happy coding - and welcome to the glorious world of beautiful bugs and endless stack traces." );
+    }
+}
